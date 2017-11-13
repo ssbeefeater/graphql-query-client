a5174e525651c172a8a044d1930314006868a3c8
mike <ssbeefeater@gmail.com>
2017-11-14 01:39:46 +0200

    add config file

---
+++ src/lib/config.js
@@ -0,0 +1,2 @@
+export const queryLockMessage = 'The request should not update, use forceFetch or set smartFetch:false in the environement options';
+export const mutationLockMessage = 'The mutation should not be executed, use forceMutate or set smartMutate:false in the environement options';
